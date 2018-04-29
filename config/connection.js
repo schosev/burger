86085b29435a1188dcd13fc4a8f9cceca5980d92
UMKC CTCFP User <umkc-ctcfpuser@umkc.edu>
2018-04-28 23:42:23 -0500

    Adding heroku mysql logic

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 var mysql = require("mysql");
 var orm = require("./orm");
+var connection;
 // create the connection information for the sql database
-var connection = mysql.createConnection({
-host: "localhost",
-port: 3306,
-user: "root",
-password: "odessa01",
-database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "odessa01",
+    database: "burgers_db"
+    });
+};
 
 // connect to the mysql server and sql database
 connection.connect(function(err) {
